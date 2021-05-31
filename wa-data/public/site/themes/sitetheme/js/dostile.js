3a3d8c256e64f885829fca5ad3ce30873fc92fd8
Vaqif <vaqif2307@gmail.com>
2021-06-01 06:47:17 +0700

    Меню "Каталог" и Страница "Продукт"(не полностью)

--- wa-data/public/site/themes/sitetheme/js/dostile.js
+++ wa-data/public/site/themes/sitetheme/js/dostile.js
@@ -1,11 +1,16 @@
   $(document).ready(function(){
 
-    $(".home_slider").owlCarousel({
+    $('.home_slider').owlCarousel({
       loop: true,
       // nav: true,
       // autoplay: true,
       items: 1
     });
+
+    $('button.catalog').on('click', function() {
+    	$('.catalog_menu').slideToggle();
+    	$('.slider').slideToggle();
+    })
 
   });
 
