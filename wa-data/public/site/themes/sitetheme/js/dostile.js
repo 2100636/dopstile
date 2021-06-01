599597a6c5c9c3573661004061a4a5deb92bf222
2100636 <2100636@mail.ru>
2021-06-02 00:19:48 +0700

    тег {strip}, Небольшие ошибки

--- wa-data/public/site/themes/sitetheme/js/dostile.js
+++ wa-data/public/site/themes/sitetheme/js/dostile.js
@@ -1,15 +1,18 @@
   $(document).ready(function(){
 
-    $('.home_slider').owlCarousel({
-      loop: true,
-      // nav: true,
-      // autoplay: true,
-      items: 1
-    });
+
+    if($('.home_slider')[0]){
+      $('.home_slider').owlCarousel({
+        loop: true,
+        // nav: true,
+        // autoplay: true,
+        items: 1
+      });
+    }
 
     $('button.catalog').on('click', function() {
     	$('.catalog_menu').slideToggle();
-    	$('.slider').slideToggle();
+    	// $('.slider').slideToggle();
     })
 
   });
